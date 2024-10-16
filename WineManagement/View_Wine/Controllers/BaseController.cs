480cdcc33e824834d69df50afded23f21aa38500
Nguyen Duc Anh <anhndse173319@fpt.edu.vn>
2024-10-16 14:45:56 +0700

    feat:db commit and code FE

---
+++ WineManagement/View_Wine/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Configuration;
+using System.Net.Http.Headers;
+using View_Wine.Models;
+
+namespace View_Wine.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly HttpClient _httpClient;
+        protected readonly string WineURL;
+
+        public BaseController(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            _httpClient.DefaultRequestHeaders.Accept.Add(contentType);
+            var apiSettings = new ApiSettings();
+            configuration.GetSection("ApiUrls").Bind(apiSettings);
+            WineURL = apiSettings.WineURL;
+
+        }
+    }
+}
